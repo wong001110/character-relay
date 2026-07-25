563e569ace502181d917dcbc15391b78f792c999
Wong Juen An <62807108+wong001110@users.noreply.github.com>
2026-07-26 02:06:22 +0800

    phase 11: mount bilingual UI provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { I18nProvider } from "./i18n";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode><App /></StrictMode>
+  <StrictMode>
+    <I18nProvider>
+      <App />
+    </I18nProvider>
+  </StrictMode>
 );
