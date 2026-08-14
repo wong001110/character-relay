73161aca2ff4b2af3f75af0df8e9468b75cd3173
Wong Juen An <62807108+wong001110@users.noreply.github.com>
2026-08-14 15:04:39 +0800

    style: keep behavior notebook overrides last

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,7 +7,6 @@ import "./interactionSessions.css";
 import "./smartParticipation.css";
 import "./toolCalling.css";
 import "./portal-v2-refine.css";
-import "./behavior-notebook-turns.css";
 import "./deployment-notebook-v2.css";
 import "./deployment-notebook-tabs.css";
 import "./deployment-paper-tags.css";
@@ -18,6 +17,7 @@ import "./key-groups-notebook.css";
 import "./key-groups-bulk-apply-v2.css";
 import "./semantic-routing-admin.css";
 import "./utility-gateway.css";
+import "./behavior-notebook-turns.css";
 import { I18nProvider } from "./i18n";
 import { SemanticRoutingJudgeDock } from "./SemanticRoutingJudgeDock";
 
